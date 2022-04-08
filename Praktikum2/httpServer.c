ead709fc7a0f6db9fc0829f9f4217bad543b69af
PaulDieterich <Paul.dieterich0@gmail.com>
2022-04-08 13:30:20 +0200

    praktilum2 fertig

--- Praktikum2/httpServer.c
+++ Praktikum2/httpServer.c
@@ -1,13 +1,12 @@
 /*
-* Autor(en):    Paul Dieterich, Christina B.
-* Praktikum:    Verteilte Systeme WiSe 2021
-* Programm:     Einfacher TCP Web-Server
-*
-* Hinweise zum kompilieren/starten:
-*       Kompilieren:    gcc -o HTTPServer_TCP HTTPServer_TCP.c
-*       Starten:        ./HTTPServer_TCP <DIRECTORY>  |OPTIONAL: <PORT>|
-*/
-
+ * Autor(en):    Paul Dieterich, Lukas  P
+ * Praktikum:    Verteilte Systeme SoSe 2022
+ * Programm:     Einfacher TCP Web-Server
+ *
+ * Hinweise zum kompilieren/starten:
+ *       Kompilieren:    g++ httpServer.c -o httpServer
+ *       Starten:        ./HTTPServer <DIRECTORY>  |OPTIONAL: <PORT>|
+ */
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
@@ -22,35 +21,41 @@
 #define MAX_SOCK 10
 #define MAXLINE 30000
 
-void getRequest(char *request, char *dateiName, char *dir_path);
-void getContentType(char *dateiName, char *contentType, char *status, char *file_path, char *dir_path);
-void request(int socket, char *dir_path);
+void getRequest(char *request, char *dateiName, char *path);
+void getContentType(char *dateiName, char *contentType, char *status, char *dateipath, char *path);
+void request(int socket, char *path);
 void sendHeader(int socket, char *status, char *type);
-void sendBody(int socket, DIR *dir, char *status, char *type, char *dir_path, char *content, char *dateiName);
+void sendBody(int socket, DIR *dir, char *status, char *type, char *path, char *content, char *dateiName);
 void err_abort(char *str);
 
-
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
     int server_fd, new_socket;
     int port;
     long valread;
-    struct sockaddr_in address;
+    struct sockaddr_in address, client;
     int addrlen = sizeof(address);
     int reuse = 1;
     char *dir_path = NULL;
-
+    pid_t child;
     if (argc < 2)
     {
         err_abort((char *)"es fehlen argumente");
     }
     dir_path = argv[1];
-    if (argc == 3){port = atoi(argv[2]);}
-    else{ port = PORT;}
+    if (argc == 3)
+    {
+        port = atoi(argv[2]);
+    }
+    else
+    {
+        port = PORT;
+    }
 
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
     {
-         err_abort((char *)"In socket");
+        perror("In socket");
+        exit(EXIT_FAILURE);
     }
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0)
     {
@@ -60,7 +65,11 @@ int main(int argc, char const *argv[])
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(port);
-
+    // Socketoption zum sofortigen Freigeben der Sockets nach Beenden des Servers
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0)
+    {
+        fprintf(stderr, "Kann Socketoption nicht setzen!");
+    }
     memset(address.sin_zero, '\0', sizeof address.sin_zero);
 
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
@@ -73,19 +82,32 @@ int main(int argc, char const *argv[])
     printf((char *)"Web-Server: bereit... \n");
     while (1)
     {
+        addrlen = sizeof(client);
         printf("\n+++++++ Waiting for new connection ++++++++\n\n");
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&client, (socklen_t *)&addrlen)) < 0)
         {
-            perror("In accept"); exit(EXIT_FAILURE);
+            perror("In accept");
+            exit(EXIT_FAILURE);
         }
-       
-        request(new_socket, dir_path);
-
+        
+        if ((child = fork()) < 0)
+        {
+            fprintf(stderr, "fork error\r\n");
+            exit(0);
+        }
+        else if (child == 0)
+        {
+            close(server_fd);
+            request(new_socket, dir_path);
+            
+            exit(0);
+        }
         close(new_socket);
         printf("\n+++++++ Socket Closed ++++++++\n\n");
     }
     return 0;
 }
+
 void request(int socket, char *dir_path)
 {
     DIR *dir = NULL;
@@ -99,20 +121,19 @@ void request(int socket, char *dir_path)
 
     long valread = read(socket, request, sizeof(request));
     printf("%s\n", request);
-   
+
     getRequest(request, dateiName, dir_path);
-   
+
     getContentType(dateiName, contentType, status, file_path, dir_path);
-  
+
     sendHeader(socket, status, contentType);
 
     sendBody(socket, dir, status, contentType, dir_path, content, dateiName);
 
-   // write(socket, content, strlen(content));
+    write(socket, content, strlen(content));
     printf("write to socket - content: %s", content);
 }
 
-//mit Hilfe von lukas Lübberding
 void getRequest(char *request, char *dateiName, char *dir_path)
 {
     char *pattern1 = "GET ";
@@ -135,14 +156,14 @@ void getRequest(char *request, char *dateiName, char *dir_path)
     }
     strcpy(dateiName, target);
     printf("getRequest %s\n", dateiName);
+    free(target);
 }
-
 void getContentType(char *dateiName, char *contentType, char *status, char *file_path, char *dir_path)
 {
     char tmp[1000] = {0};
     char stat[1000] = {0};
     char type[1000] = {0};
-    struct stat *buf;
+    struct stat buf;
     snprintf(tmp, sizeof(tmp), "%s%s", file_path, dateiName);
     strcpy(file_path, tmp);
     printf("getContent %s\n", file_path);
@@ -155,7 +176,7 @@ void getContentType(char *dateiName, char *contentType, char *status, char *file
     {
         strcpy(stat, "404 ERROR");
     }
-    printf("getContent path %s\n", dir_path); //dir_path
+    printf("getContent path %s\n", file_path);
     char *extension = strrchr(file_path, '.');
     printf("getContent extension %s\n", extension);
     if (extension)
@@ -170,7 +191,7 @@ void getContentType(char *dateiName, char *contentType, char *status, char *file
         }
         else if (strstr(extension, "jpg"))
         {
-            strcpy(type, "image/jpg");
+            strcpy(type, "image/jpeg");
         }
     }
     else
@@ -179,37 +200,37 @@ void getContentType(char *dateiName, char *contentType, char *status, char *file
     }
     strcpy(status, stat);
     strcpy(contentType, type);
-   
-    
-
+    strcat(dir_path, file_path);
 }
 
 void sendHeader(int socket, char *status, char *type)
 {
     char sh[1000] = {0};
-    snprintf(sh, sizeof(sh), "HTTP/1.1 %s\r\nContent-Type: %s\r\n\r\n", status, type);
+    snprintf(sh, sizeof(sh), "HTTP/1.1 %s\r\nContent-Type: %s\r\n", status, type);
     write(socket, sh, sizeof(sh));
     printf("sendHeader %s\n", sh);
 }
 
 void sendBody(int socket, DIR *dir, char *status, char *type, char *file_path, char *content, char *dateiName)
 {
-
-    dir = opendir(file_path);
     int content_length = 0;
     char response[MAXLINE] = {0};
-    printf("sendBody %s\n", dateiName);
+
+    printf("sendBody %s\n", file_path);
+    int l;
     if (strstr(status, "200"))
     {
-        if (strstr(type, "text/html") || strstr(type, "text/plane"))
+        if (strstr(type, "text/html") || strstr(type, "text/plain"))
         {
             char *extension = strrchr(file_path, '.');
             printf("sendBody extension %s\n", extension);
             if (extension)
             {
+
                 FILE *file = fopen(file_path, "r");
                 fread(content, 2000, 2000, file);
-                write(socket, content, strlen(content));
+                snprintf(response, sizeof(response), "Content-Length: %ld\r\n\r\n", content);
+                write(socket, response, strlen(response));
                 fclose(file);
             }
             else
@@ -221,70 +242,65 @@ void sendBody(int socket, DIR *dir, char *status, char *type, char *file_path, c
                 char filepath[MAXLINE] = {0};
                 char url[MAXLINE] = {0};
 
-               
                 strcpy(content, "<h1>Verzeichnisse</h1>");
-                
+
                 while (entry = readdir(dir))
                 {
-                   
+
                     if (!strcmp(entry->d_name, "..") || !strcmp(entry->d_name, "."))
                         continue;
 
-                   
                     sprintf(url, "%s%s", file_path, entry->d_name);
 
                     stat(url, &attribut);
                     sprintf(url, "%s", dateiName);
-                  
+
                     strcat(url, entry->d_name);
-                      printf("sendBody url %s\n", url);
+                    
                     if (S_ISDIR(attribut.st_mode))
                     {
                         strcat(url, "/");
                     }
-
-                    
+                    printf("sendBody url %s\n", url);
                     sprintf(data, "<p><a href=\"%s\">%s</a></p>", url, entry->d_name);
 
-    
                     strcat(content, data);
-                    
                 }
-                write(socket, content, strlen(content));
+                snprintf(response, sizeof(response), "Content-Length: %ld\r\n\r\n", content);
+                write(socket, response, strlen(response));
+               // write(socket, content, strlen(content));
             }
         }
-       else if (strstr(type, "image/jpg")) {
-            printf("sendBody type: image/jpg\n");
+        else if (strstr(type, "image/jpeg"))
+        {
             int buffer[512] = {0};
-
-            FILE* file = fopen(file_path, "rb");
-            printf(file);
-            fseek (file , 0 , SEEK_END);
+            FILE *file = fopen(file_path, "rb");
+            fseek(file, 0, SEEK_END);
             long lSize = ftell(file);
             fseek(file, 0, SEEK_SET);
 
-        
             char response[MAXLINE] = {0};
-            while(fread(buffer, sizeof(int), sizeof(buffer)/sizeof(int), file))
+            snprintf(response, sizeof(response), "Content-Length: %ld\r\n\r\n", lSize);
+            write(socket, response, strlen(response));
+            int rv_fread = 0;
+            while ((rv_fread = fread(buffer, 1, sizeof(buffer), file)) > 0)
             {
-            
-                if(write(socket, buffer, sizeof(buffer)) != sizeof(buffer))
-                {
-                
-                    printf("image error\r\n");
-                    return;
-                }
+                content = (char *)buffer;
+                write(socket, content, rv_fread);
+            }
+            if (rv_fread == -1)
+            {
+                printf("image error\r\n");
+                return;
             }
             fclose(file);
-        }else
-        {
-           
-            snprintf(response, sizeof(response), "Content-Length: 9\r\n\r\nERROR 404");
-            write(socket, response, strlen(response));
         }
-
     }
-    printf("\n ende sendbody\n");
+    else
+    {
+        snprintf(response, sizeof(response), "Content-Length: 9\r\n\r\nERROR 404");
+        write(socket, response, strlen(response));
+    }
 }
 void err_abort(char *str)
 {
